db0726d5a739be159e698398d6cc0bb1c2606f08
David Kroll <dkroll713@gmail.com>
2023-02-09 14:29:56 -0600

    first commit

---
+++ src/Views/Home.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Home = () => {
+
+  return (
+    <div className="home">
+      <div>
+        <h3>Easily find contributions to make</h3>
+      </div>
+      <div>
+        <div className="searchBar">
+          <input className=""></input>
+        </div>
+        <div>
+          ...results
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Home;
---
+++ src/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import { createRoot } from "react-dom/client";
+const root = createRoot(document.getElementById("root"));
+
+import Home from './Views/Home.jsx'
+
+// Huzzah for jsx!
+
+root.render(<Home />);
